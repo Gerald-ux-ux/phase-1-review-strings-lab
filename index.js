b67eb8725565816bbcde8841a11d9b8274576419
gerald <you@example.com>
2022-08-15 14:54:21 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = "Gerald"
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const  excitedWelcomeMessage = "WELCOME TO FLATBOOK, GERALD!"
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+      
